1cac26bc7b61c6f64f2e51ae16bd287ffc6a3d33
Shevchenko Taras <back.neomind@gmail.com>
2012-05-22 20:02:47 +0300

    fix upload

--- public/upload.php
+++ public/upload.php
@@ -1,5 +1,14 @@
 <?php
-if (is_uploaded_file($_FILES["fieldname"]["tmp_name"])) {
+
+	s3 = Services_Amazon_S3::getAccount("AKIAJZPI6TYUPBP73PXQ", "FUkx02+w97CjK6oWy36iUy4bqVcpU4YubzGBvIFZ");
+	bucket = $s3->getBucket("mysterybox_bucket");
+	object = $bucket->getObject($_FILES["fieldname"]["name"]);
+	object->acl = "public-read";
+	object->contentType = $_FILES["fieldname"]["type"];
+	object->data = file_get_contents($_FILES["fieldname"]["tmp_name"]);
+	object->save();
+
+/*if (is_uploaded_file($_FILES["fieldname"]["tmp_name"])) {
 	Services_Amazon_S3_Stream::register();
 	$context = stream_context_create(array(
 	
@@ -12,5 +21,5 @@ if (is_uploaded_file($_FILES["fieldname"]["tmp_name"])) {
 	copy($_FILES["fieldname"]["tmp_name"],
 		"s3://mysterybox_bucket/" . $_FILES["fieldname"]["name"],
 		$context);
-}
+}*/
 ?>

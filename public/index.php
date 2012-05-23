0937ace9635dbda8a5622c28773b0afdf0f34373
Shevchenko Taras <back.neomind@gmail.com>
2012-05-23 09:47:43 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -1,6 +1,15 @@
 <?php
-    print("Mysterybox is here! ;)\n");
-
+    print("Mysterybox is here! ;)<br>");
+	
+	if (class_exists('S3'))
+	{
+		print("s3 class exist");
+	}
+	else
+	{
+		print("warning: s3 class not exists<br>");
+	}
+	
 	if(isset($_FILES['theFile']))
 	{
 	    print("Success!");
@@ -9,13 +18,13 @@
       	print("mime: " . $_FILES['theFile']['type'] . " ");
       	print("name: " . $_FILES['theFile']['name'] . " ");
 	
-		/*s3 = Services_Amazon_S3::getAccount("AKIAJZPI6TYUPBP73PXQ", "FUkx02+w97CjK6oWy36iUy4bqVcpU4YubzGBvIFZ");
+		s3 = Services_Amazon_S3::getAccount("AKIAJZPI6TYUPBP73PXQ", "FUkx02+w97CjK6oWy36iUy4bqVcpU4YubzGBvIFZ");
 		bucket = $s3->getBucket("mysterybox_bucket");
 		object = $bucket->getObject($_FILES["theFile"]["name"]);
 		object->acl = "public-read";
 		object->contentType = $_FILES["theFile"]["type"];
 		object->data = file_get_contents($_FILES["theFile"]["tmp_name"]);
-		object->save();*/
+		object->save();
 
 		/*if (is_uploaded_file($_FILES["theFile"]["tmp_name"])) {
 			Services_Amazon_S3_Stream::register();
@@ -30,8 +39,8 @@
 	
 			copy($_FILES["theFile"]["tmp_name"],
 				"s3://mysterybox_bucket/" . $_FILES["theFile"]["name"],
-				$context);
-		}*/	
+				$context);*/
+		}	
 	}
 	else
 	{

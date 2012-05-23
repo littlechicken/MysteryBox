14b340bcba10c5048ee87053b53c75e303b1c874
Shevchenko Taras <back.neomind@gmail.com>
2012-05-23 08:45:26 +0300

    merge

--- public/index.php
+++ public/index.php
@@ -1,5 +1,40 @@
 <?php
+    echo "Mysterybox is here! ;)";
 
-echo "Mysterybox is here! ;)";
+	if(isset($_FILES['theFile']))
+	{
+	    echo "Success!";
+	    print("tmpName: " . $_FILES['theFile']['tmp_name'] . " ");
+      	print("size: " . $_FILES['theFile']['size'] . " ");
+      	print("mime: " . $_FILES['theFile']['type'] . " ");
+      	print("name: " . $_FILES['theFile']['name'] . " ");
+	
+		s3 = Services_Amazon_S3::getAccount("AKIAJZPI6TYUPBP73PXQ", "FUkx02+w97CjK6oWy36iUy4bqVcpU4YubzGBvIFZ");
+		bucket = $s3->getBucket("mysterybox_bucket");
+		object = $bucket->getObject($_FILES["theFile"]["name"]);
+		object->acl = "public-read";
+		object->contentType = $_FILES["theFile"]["type"];
+		object->data = file_get_contents($_FILES["theFile"]["tmp_name"]);
+		object->save();
+	}
+	else
+	{
+	    echo "Failed!";
+	}
 
-?> 
+
+/*if (is_uploaded_file($_FILES["fieldname"]["tmp_name"])) {
+	Services_Amazon_S3_Stream::register();
+	$context = stream_context_create(array(
+	
+	"s3" => array(
+	"access_key_id" => "AKIAJZPI6TYUPBP73PXQ",
+	"secret_access_key" => "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",
+	"content_type" => $_FILES["fieldname"]["type"],
+	"acl" => "public-read")));
+	
+	copy($_FILES["fieldname"]["tmp_name"],
+		"s3://mysterybox_bucket/" . $_FILES["fieldname"]["name"],
+		$context);
+}*/
+?>

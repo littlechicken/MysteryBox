b04c8f9fff02fd4ec5aaecfd5b9ad1925d8f7f40
Shevchenko Taras <back.neomind@gmail.com>
2012-06-09 11:12:48 +0300

    added jsmovie and seq

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,20 +10,28 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
+
+    }
+
+    public function pagesAction()
+    {
     	$vmap = new Application_Model_ViewerMapper();
     	$v = new Application_Model_Viewer();
-    	
+    	 
     	$map = new Application_Model_BoxMapper();
-    	
+    	 
     	$entries   = array();
     	$viewers = $vmap->fetchAll();
-    		
-    	$this->view->entries = $viewers;    	
+    	
+    	$this->view->entries = $viewers;
     }
 
-    public function exampleAction()
-    {
 
-    }
 }
+
+
+
+
+
+
 
